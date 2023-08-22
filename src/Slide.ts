92f08062df5c44de8466e43b7edbaad204c33857
marcelopajr <marcelo96jr@gmail.com>
2023-08-21 23:00:01 -0300

    fix: add final adjustments

--- src/Slide.ts
+++ src/Slide.ts
@@ -100,6 +100,8 @@ export default class Slide {
   }
 
   pause() {
+    document.body.classList.add("paused");
+
     this.pausedTimeout = new Timeout(() => {
       this.timeout?.pause();
       this.paused = true;
@@ -110,6 +112,8 @@ export default class Slide {
   }
 
   continue() {
+    document.body.classList.remove("paused");
+
     this.pausedTimeout?.clear();
 
     if (this.paused) {
@@ -130,14 +134,11 @@ export default class Slide {
     this.controls.appendChild(nextButton);
 
     this.controls.addEventListener("pointerdown", () => this.pause());
-    this.controls.addEventListener("pointerup", () => this.continue());
+    document.addEventListener("pointerup", () => this.continue());
+    document.addEventListener("touchend", () => this.continue());
 
-    prevButton.addEventListener("pointerup", () => {
-      this.prev();
-    });
-    nextButton.addEventListener("pointerup", () => {
-      this.next();
-    });
+    prevButton.addEventListener("pointerup", () => this.prev());
+    nextButton.addEventListener("pointerup", () => this.next());
   }
 
   private addThumbItems() {
