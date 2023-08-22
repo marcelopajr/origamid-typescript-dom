6713e2d894d3acae83179f11f1c6538e461f7d26
marcelopajr <marcelo96jr@gmail.com>
2023-08-21 21:47:10 -0300

    feat: add advanced timeout function

--- src/Slide.ts
+++ src/Slide.ts
@@ -8,6 +8,8 @@ export default class Slide {
   index: number;
   slide: Element;
   timeout: Timeout | null;
+  pausedTimeout: Timeout | null;
+  paused: boolean;
 
   constructor(
     container: Element,
@@ -20,8 +22,10 @@ export default class Slide {
     this.controls = controls;
     this.time = time;
     this.timeout = null;
+    this.pausedTimeout = null;
     this.index = 0;
     this.slide = this.slides[this.index];
+    this.paused = false;
 
     this.init();
   }
@@ -44,13 +48,31 @@ export default class Slide {
   }
 
   prev() {
+    if (this.paused) return;
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
     this.show(prev);
   }
 
   next() {
+    if (this.paused) return;
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
     this.show(next);
+  }
+
+  pause() {
+    this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause();
+      this.paused = true;
+    }, 300);
+  }
+
+  continue() {
+    this.pausedTimeout?.clear();
+
+    if (this.paused) {
+      this.paused = false;
+      this.timeout?.continue();
+    }
   }
 
   private addControls() {
@@ -60,6 +82,9 @@ export default class Slide {
     nextButton.innerText = "Slide Posterior";
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
+
+    this.controls.addEventListener("pointerdown", () => this.pause());
+    this.controls.addEventListener("pointerup", () => this.continue());
 
     prevButton.addEventListener("pointerup", () => {
       this.prev();
